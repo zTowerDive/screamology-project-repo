f24ac893e113e96c9d4120f1aba17d1f5421bf48
zTowerDive <MetallicGreyYT@gmail.com>
2026-04-26 20:59:23 -0400

    Adjusted flashlight, began making player hud
    Flashlight now has a chance of deactivating every few seconds or so
    
    Player now has placholder hud that can display and animate text/dialogue

--- player/flashlight_controller.gd
+++ player/flashlight_controller.gd
@@ -1,21 +1,33 @@
 class_name FlashlightController extends Node3D
 
 @onready var _spot_light: SpotLight3D = %SpotLight3D
+@onready var _light_toggle_timer: Timer = %LightToggleTimer
+
 @export var projector_texture : Texture2D = null:
 	set = set_projector_texture
 
-var _is_on := true
+## Chance the light has to deactivate expressed as a percentage
+@export var light_deactivation_chance := 0.9
+## Current toggle state of the light as a boolean
+var _is_on := true:
+	set = set_in_on
 
 func _ready() -> void:
 	_is_on = true
+	
+	_light_toggle_timer.timeout.connect(
+		func() -> void:
+			if _is_on:
+				calculate_light_state()
+	)
 
 ## Toggle the light's visible property when the player presses the "flashlight" input
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("flashlight"):
-		_spot_light.visible = not _is_on
-		_is_on = not _is_on
+		set_in_on(not _is_on)
 
 
+## Function set the light_projector property of the _spot_light at runtime
 func set_projector_texture(new_texture: Texture2D) -> void:
 	projector_texture = new_texture
 	
@@ -23,3 +35,16 @@ func set_projector_texture(new_texture: Texture2D) -> void:
 		return
 	
 	_spot_light.light_projector = projector_texture
+
+## Function sets the state of the _is_on variable and the _spot_light's visible property
+func set_in_on(new_value: bool) -> void:
+	_is_on = new_value
+	_spot_light.visible = _is_on
+
+
+func calculate_light_state()-> void:
+	var chance : float = randf()
+	print(chance)
+	
+	if chance < light_deactivation_chance:
+		set_in_on(false)
---
+++ player/player_hud.gd
@@ -0,0 +1,22 @@
+class_name PlayerHUD extends Control
+
+
+
+@onready var _current_task_label: RichTextLabel = %CurrentTaskLabel
+@onready var _dialogue_label: RichTextLabel = %DialogueLabel
+
+var _tween_text : Tween = null
+
+
+
+func show_text(text_label: RichTextLabel, duration: float) -> void:
+	if _tween_text != null:
+		_tween_text.kill()
+	
+	if text_label == null:
+		return
+	
+	_tween_text = create_tween()
+	text_label.visible_ratio = 0.0
+	
+	_tween_text.tween_property(text_label, "visible_ratio", 1.0, duration)
