18e1c77d3fd3b66ab75b803077549bf1605df811
Dialmee <maggie.fabre02@gmail.com>
2026-04-29 19:29:32 +0200

    [UI][PauseAndMain]

--- pause/check_button.gd
+++ pause/check_button.gd
@@ -7,7 +7,7 @@ extends TextureButton
 
 func _on_mouse_entered() -> void:
 	textureCheck.self_modulate = Color(1.61, 1.61, 1.61, 1.0)
-	var tween = get_tree().create_tween()
+	var tween = get_tree().create_tween().set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
 	tween.tween_property(textureCheck, "scale", Vector2(0.8,0.8), 0.1).set_trans(Tween.TRANS_BOUNCE)
 	tween.tween_property(textureCheck, "scale", Vector2(1.5,1.5), 0.5).set_trans(Tween.TRANS_BOUNCE)
 
