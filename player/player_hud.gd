70af2dc445ca9b0164e231e77718082028ab6c76
Dialmee <maggie.fabre02@gmail.com>
2026-04-29 21:18:40 +0200

    [UI]playerHudBoxesChanged

--- player/player_hud.gd
+++ player/player_hud.gd
@@ -1,14 +1,15 @@
 class_name PlayerHUD extends Control
 
 
-
+@onready var tasks_rect: TextureRect = $tasksRect
 @onready var _current_task_label: RichTextLabel = %CurrentTaskLabel
 @onready var _dialogue_label: RichTextLabel = %DialogueLabel
+@onready var dialogue_rect: TextureRect = $dialogueRect
 
 var _tween_text : Tween = null
 
 func _ready() -> void:
-	_dialogue_label.visible = false
+	dialogue_rect.visible = false
 
 
 func show_dialogue_text(dialogue_item: DialogueItem, duration: float) -> void:
@@ -19,7 +20,7 @@ func show_dialogue_text(dialogue_item: DialogueItem, duration: float) -> void:
 		return
 	
 	_tween_text = create_tween()
-	_dialogue_label.visible = true
+	dialogue_rect.visible = true
 	_dialogue_label.text = dialogue_item.text
 	_dialogue_label.visible_ratio = 0.0
 	
@@ -27,7 +28,7 @@ func show_dialogue_text(dialogue_item: DialogueItem, duration: float) -> void:
 	_tween_text.finished.connect(
 		func() -> void:
 			await get_tree().create_timer(1.0).timeout
-			_dialogue_label.visible = false
+			dialogue_rect.visible = false
 	)
 
 
